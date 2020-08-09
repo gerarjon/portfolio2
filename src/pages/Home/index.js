02bdefc2d6188edbf74d5d78d14bf21a9e4a3c4b
gerarjon <gerarsuaverdez@gmail.com>
2020-08-08 22:39:16 -0700

    added skills section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../../components/Hero'
 import ImageGrid from '../../components/ImageGrid';
+import Footer from '../../components/Footer';
 
 function Home() {
   return(
     <>
       <Hero />
       <ImageGrid />
+      <Footer />
     </>
   )
 }
