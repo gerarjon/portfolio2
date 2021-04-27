fb6d5d3a38d2cac4d40c328fe282d21ecb25c297
gerarjon <gerarsuaverdez@gmail.com>
2021-04-27 15:20:39 -0700

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Gerar Suaverdez - Portfolio
-
-## About 
-Welcome to my portfolio. In here you can find the latest projects I have worked on individually or as a group. 
-
-## Contact
-* [GitHub](https://github.com/gerarjon)
-* Email: gerarsuaverdez@gmail.com
---
+++ precache-manifest.dea4d5c1c9b63b4247bb8e3290c5c69a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe81f1adc6734c8e0d4771729195fff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ff8de41871351d4942",
+    "url": "/static/css/main.a835530a.chunk.css"
+  },
+  {
+    "revision": "40191756623d408559d2",
+    "url": "/static/js/2.500e1402.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.500e1402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ff8de41871351d4942",
+    "url": "/static/js/main.65551aa9.chunk.js"
+  },
+  {
+    "revision": "75f65fa61937d16649bc",
+    "url": "/static/js/runtime-main.d2b1bba4.js"
+  }
+]);
