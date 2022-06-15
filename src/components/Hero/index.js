7b7bf5e6b6dd36b36b99d7283800f921f08f267b
gerarjon <gerarsuaverdez@gmail.com>
2022-06-15 11:01:57 -0700

    change icons and colors of social media icons

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -9,16 +9,16 @@ function Hero() {
           Gerar Suaverdez
         </h5>
         <h1>
-          Full Stack
-          <br />
-          Web Developer
+          Full Stack 
+          <br></br>
+          Developer
         </h1>
-        <ul>
-          <li><a href="https://github.com/gerarjon" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/gerar-suaverdez-002351177/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a></li>
-          <li><a href="mailto: gerarsuaverdez@gmail.com"><i className="fas fa-envelope-square"></i></a></li>
-          <li><a href="https://www.instagram.com/gerar_jon/" target="_blank" rel="noopener noreferrer">Art</a></li>
-          <li><a href="assets/documents/GSuaverdez_Resume.pdf" target="_blank">Resumé</a></li>
+        <ul className='icons'>
+          <li><a className='icon icon--github' href="https://github.com/gerarjon" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></li>
+          <li><a className='icon icon--linkedin' href="https://www.linkedin.com/in/gerar-suaverdez-002351177/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a></li>
+          <li><a className='icon icon--email' href="mailto: gerarsuaverdez@gmail.com"><i className="fas fa-envelope-square"></i></a></li>
+          <li><a className='icon icon--instagram' href="https://www.instagram.com/gerar_jon/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a></li>
+          <li><a className='icon icon--resume' href="assets/documents/GSuaverdez_Resume.pdf" target="_blank"><i className="fas fa-file"></i></a></li>
         </ul>
       </div>
     </section>
